2edb1205e8e74b83e6ba468eeb2c30bcd964dfb9
ro25-wq <maximogael2935@gmail.com>
2026-06-24 22:39:33 -0400

    Cambio Ep3

--- usuarios-clientes-api/src/test/java/cl/duocuc/masterbikes/usuarios_clientes_api/UsuariosClientesApiApplicationTests.java
+++ usuarios-clientes-api/src/test/java/cl/duocuc/masterbikes/usuarios_clientes_api/UsuariosClientesApiApplicationTests.java
@@ -3,9 +3,11 @@ package cl.duocuc.masterbikes.usuarios_clientes_api;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @Disabled("Requiere conexión a Oracle — se ejecuta solo con la Wallet disponible")
 @SpringBootTest
+@ActiveProfiles("test")
 class UsuariosClientesApiApplicationTests {
 
 	@Test
@@ -13,3 +15,7 @@ class UsuariosClientesApiApplicationTests {
 	}
 
 }
+
+
+
+
